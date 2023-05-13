ea3275e6e3eda4c110b8f29e6ad8705b08201fd4
PuypoDeveloper <estebanlpe@gmail.com>
2023-05-13 17:05:07 -0500

    ignore

---
+++ front/.next/server/next-font-manifest.js
@@ -0,0 +1,13 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/": [
+      "static/media/c95a3d7e5d1d39ce-s.p.woff2"
+    ],
+    "/visualUser": [
+      "static/media/c95a3d7e5d1d39ce-s.p.woff2"
+    ]
+  },
+  "app": {},
+  "appUsingSizeAdjust": false,
+  "pagesUsingSizeAdjust": true
+}
